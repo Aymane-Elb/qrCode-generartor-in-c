c4e171c8242d322fc8beb9769d250170602b8df2
Aymane-Elb <aymaneelabdi614@gmail.com>
2026-03-04 23:02:25 +0000

    boom

--- qr.c
+++ qr.c
@@ -4,10 +4,17 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define SCALE 10
+#define Qr_max 2953
 
 int main() {
+
   uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
   uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+  char inputText[Qr_max];
+  fgets(inputText, Qr_max, stdin);
+  memset(inputText, 0, Qr_max);
   bool ok = qrcodegen_encodeText(
       "Hello, world!", tempBuffer, qr0, qrcodegen_Ecc_MEDIUM,
       qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
@@ -23,7 +30,7 @@ int main() {
   printf("size%d\n", size);
   struct TinyPngOut writer;
   enum TinyPngOut_Status init_status =
-      TinyPngOut_init(&writer, size, size, file);
+      TinyPngOut_init(&writer, size * SCALE, size * SCALE, file);
   if (init_status != TINYPNGOUT_OK) {
     printf("couldnt initialize tinyOut write%d\n", init_status);
     exit(-1);
@@ -31,9 +38,9 @@ int main() {
   uint8_t rgb_black[] = {0, 0, 0};
   uint8_t rgb_white[] = {255, 255, 255};
 
-  for (int y = 0; y < size; y++) {
-    for (int x = 0; x < size; x++) {
-      bool color = qrcodegen_getModule(qr0, x, y);
+  for (int y = 0; y < size * SCALE; y++) {
+    for (int x = 0; x < size * SCALE; x++) {
+      bool color = qrcodegen_getModule(qr0, x / SCALE, y / SCALE);
       TinyPngOut_write(&writer, color ? rgb_white : rgb_black, 1);
     }
   }
